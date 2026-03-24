ae15793259e2ed0763a99d1797d2e268860a9347
Antonio Bonet <tonybonet31@yahoo.es>
2026-03-25 02:30:46 +0700

    fix: remove carets from dependencies and fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite-plus'
 
 export default defineConfig({
-  staged: {
-    "*": "vp check --fix"
+  fmt: {
+    singleQuote: true,
+    semi: false,
   },
-  lint: {"options":{"typeAware":true,"typeCheck":true}},
-});
+
+  lint: {
+    ignorePatterns: [
+      '**/dist/**',
+      '**/coverage/**',
+      '**/.vite/**',
+      '**/playwright-report/**',
+      '**/test-results/**',
+    ],
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+
+  staged: {
+    '*.{js,ts,tsx,jsx,cjs,mjs,css,md,json,yml,yaml}': 'vp check --fix',
+  },
+})
